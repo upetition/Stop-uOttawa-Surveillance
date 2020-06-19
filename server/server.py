4b92b31c1a5bd7393ee9d7e9ce881e09384aecaa
Natalia Maximo <natalia.maximo@uottawa.ca>
2020-06-19 17:02:20 -0400

    new: added skeleton routes

--- server/server.py
+++ server/server.py
@@ -15,6 +15,46 @@ def catch_all(path):
     return send_from_directory(app.static_folder, file_to_serve)
 
 
+# Route to get the text copy of a page
+@app.route('/api/copy/<page:str>')
+def get_copy_for(page):
+    '''
+    Route to get the text copy of a page
+    '''
+    pass
+
+
+@app.route('/api/add/student')
+def add_student():
+    '''
+    Extracts the name, student number, and uottawa email from the request body
+    and adds it to the database. Also triggers a job to send the confirmation email
+    to the student to verify that they are actually a uottawa student
+    '''
+
+
+@app.route('/api/verify/<id:str>')
+def verify_student(id):
+    '''
+    Uses the ID that was sent to the student's email to set them as confirmed
+    in the database.
+    '''
+
+
+@app.route('/api/delete/<student_number:int>')
+def delete_student(student_number):
+    '''
+    Uses the student number to trigger the deletion process for a student from the database.
+    '''
+
+
+@app.route('/api/count/verified')
+def count_verified():
+    '''
+    Get the total number of verified petition signers
+    '''
+
+
 # Error Handler
 @app.errorhandler(404)
 def page_not_found(error):

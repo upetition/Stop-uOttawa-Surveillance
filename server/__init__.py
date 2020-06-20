bd35f993fdf338e1f30cb75400867753076dc38a
Natalia Maximo <natalia.maximo@uottawa.ca>
2020-06-20 17:26:16 -0400

    chg: isolate db type into its own env var

--- server/__init__.py
+++ server/__init__.py
@@ -9,9 +9,9 @@ logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__, static_folder='build')
 db: DatabaseDriver = None
 
-if CONSTANTS['ENV'] == 'development':
-    logging.debug('Development environment, using Mongo driver.')
-    logging.warning('Do not use this in production')
+if CONSTANTS['DB_TYPE'] == 'local':
+    logging.warning('DB type is local, using Mongo driver.')
+    logging.warning('Do not use this in production!!')
     db = MongoDriver(
         app,
         CONSTANTS['DB_HOST'],

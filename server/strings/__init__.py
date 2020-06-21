14de75847d05e2573103327fc46cae3e41b7a501
Natalia Maximo <natalia.maximo@uottawa.ca>
2020-06-20 22:00:41 -0400

    new: added basic strings for general structure

---
+++ server/strings/__init__.py
@@ -0,0 +1,69 @@
+
+strings = {
+    'menus': {
+        'privacy_link': {
+            'en': 'Privacy Statement',
+            'fr': ''
+        },
+        'home_page': {
+            'en': 'Home Page',
+            'fr': ''
+        },
+        'petition': {
+            'en': 'Read Petition',
+            'fr': ''
+        },
+        'github_link': {
+            'en': 'See the code for this website on '
+            '<a href="https://github.com/DudeRandom21/Stop-uOttawa-Surveillance">GitHub</a>',
+            'fr': ''
+        }
+    },
+    'petition_form': {
+        'title_text': {
+            'en': 'Lorem Ipsum',
+            'fr': 'Lorem Ipsum'
+        },
+        'name_field_title': {
+            'en': 'Name',
+            'fr': 'Nom',
+        },
+        'email_field_title': {
+            'en': 'Email',
+            'fr': 'Email'
+        },
+        'student_number_field_title': {
+            'en': 'Student number',
+            'fr': 'Numéro d\'étudiant',
+        },
+        'privacy_statement_disclaimer': {
+            'en': 'I have read and accept the privacy policy',
+            'fr': '',
+        },
+        'petition_disclaimer': {
+            'en': 'I have read the petition and provided my name, email and '
+                  'student number to digitally sign it',
+            'fr': ''
+        },
+    },
+    'privacy_page': {
+        'privacy_page_title': {
+            'en': 'Privacy Statement',
+            'fr': '',
+        },
+        'privacy_statement_body': {
+            'en': 'Lorem ipsum dolor sit amet',
+            'fr': ''
+        }
+    },
+    'petition_page': {
+        'petition_title': {
+            'en': '',
+            'fr': '',
+        },
+        'petition_body': {
+            'en': '',
+            'fr': ''
+        }
+    },
+}

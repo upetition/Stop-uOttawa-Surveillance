54377cefe385f295581fc81d7efb4aa0c903c2a0
Natalia Maximo <natalia.maximo@uottawa.ca>
2020-07-28 19:50:51 -0400

    chg: changed certbot to run instead of exec

--- run-prod.sh
+++ run-prod.sh
@@ -4,4 +4,4 @@ docker network create --driver bridge router_router || true;
 cat .token | docker login https://docker.pkg.github.com -u $GITHUB_USER --password-stdin;
 docker-compose -f docker/prod.yml pull;
 docker-compose -f docker/prod.yml up -d;
-docker-compose -f docker/router-prod.yml exec certbot renew;
+docker-compose -f docker/router-prod.yml run certbot renew;

48ed138004996250b2bfe86d7be5d7eed1a76c70
Natalia Maximo <natalia.maximo@uottawa.ca>
2020-07-28 22:24:08 -0400

    chg: split pull process for deployment

--- run-prod.sh
+++ run-prod.sh
@@ -2,6 +2,12 @@
 source .env;
 docker network create --driver bridge router_router || true;
 cat .token | docker login https://docker.pkg.github.com -u $GITHUB_USER --password-stdin;
-docker-compose -f docker/prod.yml pull;
-docker-compose -f docker/prod.yml up -d;
-docker-compose -f docker/router-prod.yml run certbot renew;
+
+# Split the pull/recreate step because otherwise we run out of memory
+docker-compose -f docker/prod.yml pull backend;
+docker-compose -f docker/prod.yml up -d backend;
+
+docker-compose -f docker/prod.yml pull frontend;
+docker-compose -f docker/prod.yml up -d frontend;
+# Certbot stuff is a little funky right now so let's shelve that
+# docker-compose -f docker/router-prod.yml run certbot renew;
